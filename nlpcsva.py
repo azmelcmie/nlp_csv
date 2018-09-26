bdf2fd67857c30242159ceaeeaad75ad3e7d9e13
azmelcmie <azmel@cluemein.com.my>
2018-09-26 12:08:19 +0800

    Initial files

---
+++ nlpcsva.py
@@ -0,0 +1,55 @@
+# Natural Language Processing
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('data/news_corpora.csv', header=None, delimiter = '\t', quoting = 3, engine='python')
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 4].values
+
+# Encoding the categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
+
+# Cleaning the texts
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+corpus = []
+for i in range(0, 1000):
+    review = re.sub('[^a-zA-Z]', ' ', dataset[1][i])
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
+
+# Creating the Bag of Words model
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features = 1500)
+X = cv.fit_transform(corpus).toarray()
+#y = dataset.iloc[:, 4].values
+
+# Splitting the dataset into the Training set and Test set
+# from sklearn.cross_validation import train_test_split #deprecated
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+# Fitting Naive Bayes to the Training set
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
